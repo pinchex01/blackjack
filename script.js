3d133b1565534c6790b12c9385d590a6df3e286c
pete <ptrmutie@gmail.com>
2019-02-06 17:03:44 +0300

    some changes

--- script.js
+++ script.js
@@ -75,9 +75,56 @@ function getNextCard(){
 
 
 // let deck = createDeck();
+
+function getCardNumericValue(card){
+  switch(card.value){
+    case 'Ace':
+      return 1;
+    case 'Two':
+      return 2;
+    case 'Three':
+      return 3;
+    case 'Four':
+      return 4;
+    case 'Five':
+      return 5;
+    case 'Six':
+      return 6;
+    case 'Seven':
+      return 7;
+    case 'Eight':
+      return 8;
+    case 'Nine':
+      return 9;
+    default:
+      return 10;
+  }
+}
   
 function getCardString(card){
   return card.value+ ' of  '+card.suit;
+}
+
+function getScore(cardArray){
+  let score = 0;
+  let hasAce = false;
+  for( let i = 0; i < cardArray.length; i++){
+    let card = cardArray[i];
+    score += getCardNumericValue(card);
+    if(card.value === 'Ace'){
+      hasAce = true
+    }
+  }
+  if(hasAce && score + 10 <= 21){
+    return score + 10;
+  }
+  return score;
+}
+
+
+function updateScores(){
+  dealerScore = getScore(dealerCards);
+  playerScore = getScore(playerCards);
 }
 
 function showStatus(){
@@ -89,6 +136,34 @@ function showStatus(){
   textArea.innerText += '\n' +getCardString(deck[i]);
   }
 }
+
+let dealerCardString = '';
+  for( let i=0; i< dealerCards.length; i++){
+    dealerCardString += getCardString(dealerCards[i]) + '\n';
+  }
+let playerCardString = '';
+  for( let i=0; i< playerCards.length; i++){
+    playerCardString += getCardString(dealerCards[i]) + '\n';  
+}
+
+updateScores();
+
+textArea.innerText =
+'Dealer has: \n' +
+dealerCardString +
+'(Score: '+ playerScore + ')\n\n';
+
+if(gameOver){
+  if(playerWon){
+    textArea.innerText = "YOU WIN"
+  }else{
+    textArea.innerText = "DEALER WINS"
+  }
+  newGameButton.style.display = 'inline';
+  hitButton.style.display = 'none';
+  stayButton.style.display = 'none';
+}
+
 
 // let playerCards = [
 //     getNextCard(), getNextCard()
